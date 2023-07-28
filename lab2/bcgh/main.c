774958f9719d80cb3ffa8dcf83cd751e1c2f1991
SNKonara <140620408+SNKonara@users.noreply.github.com>
2023-07-28 08:42:13 +0530

    Add files via upload

---
+++ lab2/bcgh/main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{ int x,y;
+
+ for(x=1; x<=5 ; x++)
+ {
+     printf("*");
+ }
+}
---
+++ lab2/exam/main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main()
+{
+  int i=0;
+  while(i<3)
+
+  printf("in while loop \n %d",i);
+  i++;
+}
+
