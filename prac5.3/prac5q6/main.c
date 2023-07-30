6bc826c59580667953dd2eb86706fecdb36cbee0
SNKonara <140620408+SNKonara@users.noreply.github.com>
2023-07-30 10:33:22 +0530

    Add files via upload

---
+++ prac5.3/prac5q6/main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+ int base,expo,power=1;
+
+ printf("enter the base:");
+ scanf(" %d",&base);
+ printf("enter the exponent:");
+ scanf(" %d",&expo);
+
+ while(expo>0)
+ {
+ power=base*power;
+ expo--;
+ }
+ printf("The power of given number: %d",power);
+}
---
+++ prac5.3/prac5q7/main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+ char lett='A';
+
+
+ while(lett<='Z')
+ {
+ printf("ASCII value of %c is: %d \t",lett,lett);
+ lett++;
+ }
+}
