008fbfb9783e731bcc3dcca870604499d87c4247
SNKonara <140620408+SNKonara@users.noreply.github.com>
2023-07-30 09:02:41 +0530

    Add files via upload

---
+++ prac3.2/prac3q4/main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int main()
+{
+  float radius;
+  printf("Enter the radius:");
+  scanf("%f",&radius);
+
+  printf("Diameter is: %.2f \n",radius*2.0);
+  printf("Circumference is: %.2f \n",radius*M_PI*2.0);
+  printf("Area is: %.2f",radius*radius*2.0* M_PI);
+
+}
---
+++ prac3.2/prac3q6/main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+ char character;
+
+ printf("Enter any character:");
+ scanf(" %c",&character);
+ printf("Integer equivalent is: %d",character);
+}
