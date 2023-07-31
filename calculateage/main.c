4cd1e720ac5ec7426ad043743604a5ad1b7800e6
SNKonara <140620408+SNKonara@users.noreply.github.com>
2023-07-31 15:59:54 +0530

    Add files via upload

---
+++ calculateage/main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+  int a1,b2,total
+  printf("Enter First Number");
+  scanf("%d,&a1");
+  printf("Enter the second number");
+  scanf("&d,&b2");
+  total = a1+b2
+  printf("Total is &d \n",total);
+}
